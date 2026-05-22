05955443bde40c9c86640bc4dc6fc45d7530a478
CoderSTanvir <87088200+CoderSTanvir@users.noreply.github.com>
2026-05-22 20:40:57 +0600

    Add files via upload

---
+++ build-a-string-repeating-function.js
@@ -0,0 +1,22 @@
+** start of script.js **
+
+function repeatStringNumTimes(str, num){
+  let repeated=""
+  
+  if(num>0){
+    for(let i=0; i<num; i++){
+       repeated+=str
+    }
+  }
+  else{
+    return ""
+  }
+return repeated
+}
+
+console.log(repeatStringNumTimes("abc", 0))
+
+
+
+** end of script.js **
+

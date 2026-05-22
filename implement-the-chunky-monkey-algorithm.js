42b7817e282bfd7c0fd3637889bb0a5340f3bb92
CoderSTanvir <87088200+CoderSTanvir@users.noreply.github.com>
2026-05-22 19:31:45 +0600

    Add files via upload

---
+++ implement-the-chunky-monkey-algorithm.js
@@ -0,0 +1,15 @@
+** start of script.js **
+
+function chunkArrayInGroups(arr,num){
+  const arrn=[]
+
+  for(let i=0; i<arr.length; i+=num){
+    arrn.push(arr.slice(i, i+num))
+  }
+  return arrn
+}
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) )
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+
+** end of script.js **
+

e78faa7d0bf0195707d80de0ceef69c8590bdbbf
CoderSTanvir <87088200+CoderSTanvir@users.noreply.github.com>
2026-05-22 12:09:03 +0600

    Add files via upload

---
+++ build-a-longest-word-finder-app.js
@@ -0,0 +1,19 @@
+** start of script.js **
+
+function findLongestWordLength(string){
+  const words= string.split(" ")
+let max = 0;
+for (let i = 0; i < words.length; i++) {
+  if (words[i].length > max) {
+    max = words[i].length
+  }
+}
+return max
+}
+const string = "the quick"
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog") )
+
+
+** end of script.js **
+

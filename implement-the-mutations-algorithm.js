545caac9425ce4741f45febdae624c5f713a0fcf
CoderSTanvir <87088200+CoderSTanvir@users.noreply.github.com>
2026-05-22 18:24:44 +0600

    Add files via upload

---
+++ implement-the-mutations-algorithm.js
@@ -0,0 +1,19 @@
+** start of script.js **
+
+function mutation(arr){
+  
+  for(let i=0; i<arr[1].length; i++){
+  if(arr[0].toLowerCase().indexOf(arr[1][i].toLowerCase())==-1){
+    return false
+    }
+
+  }
+ return true
+}
+
+console.log(mutation(["hello", "Hello"]))
+const arr=["hello", "hey"]
+console.log(arr[1].indexOf(arr[0][2]))
+
+** end of script.js **
+
